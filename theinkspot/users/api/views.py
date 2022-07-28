00981b50c9e814751210e03a8b0873ff952a55af
Mariam-Elhdad <mariam.elhdad5@gmail.com>
2022-07-28 20:06:13 +0200

    Added List

--- theinkspot/users/api/views.py
+++ theinkspot/users/api/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.http import Http404
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
@@ -7,6 +8,9 @@ from rest_framework.viewsets import GenericViewSet
 from .serializers import UserSerializer, ListSerializer
 from theinkspot.users.models import List
 from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 
 User = get_user_model()
 
@@ -27,22 +31,70 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
 
 
 
-class ListCreationView(GenericViewSet):
+class ListCreationView(APIView):
     #serializer_class = ListView
 
     def post(self, request):
-        user = request.data
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        user_data = serializer.data
+        list = request.data
+        serializer = self.serializer_class(data=list)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
      
-user_list_view = ListCreationView.as_view({'get': 'post'})
+user_list_view = ListCreationView.as_view()
 
-class ListDetailsView(GenericViewSet):
-        queryset = List.objects.all()
-        serializer_class = ListSerializer
+class ListDetailsView(APIView):
+    def retrieve(self, request, pk): # adding primary key
+       # queryset = List.objects.all()
+       # list = get_object_or_404(queryset, pk=pk)
+        try:
+            list = List.objects.get(pk=pk)
+        except List.DoesNotExist:
+             raise Http404("Not exist.")
+
+        serializer = ListSerializer(list)
+        return Response(serializer.data)
         
+user_list_details_view = ListDetailsView.as_view()
 
-user_list_details_view = ListDetailsView.as_view({'get': 'serializer_class'})
 
+
+# @api_view(['GET', 'POST'])
+# def list_create(request):
+
+#     if request.method == 'GET':
+#         lists = List.objects.all()
+#         serializer = ListSerializer(lists, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         serializer = ListSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def list_details(request, pk):
+#     try:
+#         list = List.objects.get(pk=pk)
+#     except List.DoesNotExist:
+#          return Response(status=status.HTTP_404_NOT_FOUND)
+#     if request.method == 'GET':
+#          serializer = ListSerializer(list)
+#          return Response(serializer.data)
+#     elif request.method == 'PUT':
+#         serializer = ListSerializer(list, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'DELETE':
+#         list.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+     
--- theinkspot/users/urls.py
+++ theinkspot/users/urls.py
@@ -4,19 +4,20 @@ from theinkspot.users.views import (
     user_detail_view,
     user_redirect_view,
     user_update_view,
-    
 )
+
 from theinkspot.users.api.views import(
     user_list_view,
     user_list_details_view,
-)
+ )
+
 
 app_name = "users"
 urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
-    
+    #List URLs 
     path('lists/', view=user_list_view, name="user_list_view" ),
     path('lists/<int:pk>/', view=user_list_details_view, name="list_details"),
 ]
